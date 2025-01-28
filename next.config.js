f1269cd0a148eaef8efe81d864c03e5d6d89b2cb
Starmoon0911 <starmoon9159@gmail.com>
2025-01-28 15:03:19 +0800

    Remove webpack config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack(config, { isServer }) {
-    if (!isServer) {
-      config.module.rules.push({
-        test: /components\/ui/,
-        loader: 'ignore-loader',
-      });
-    }
-    return config;
-  },
+
   images: {
     domains: ['via.placeholder.com', 'localhost'],
   },
